285165e78400bf248e72521f791572bcdec4a2d2
William Pettersson <contact@williampettersson.eu>
2026-01-06 16:36:16 +0100

    Add router + unstyled navigation menu

---
+++ src/components/layout/NavMenu.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li><router-link :to="{ name: 'home' }">Startsida</router-link></li>
+      <li><router-link :to="{ name: 'albums' }">Album</router-link></li>
+      <li><router-link :to="{ name: 'info' }">Om</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from "../views/Home.vue";
+import Albums from "../views/Albums.vue";
+import Info from "../views/Info.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/albums",
+      name: "albums",
+      component: Albums
+    },
+    {
+      path: "/info",
+      name: "info",
+      component: Info
+    }
+  ]
 })
 
 export default router
