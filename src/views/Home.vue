4c5a4558d1967d5c7dc9ea06a0443c97b5818388
William Pettersson <contact@williampettersson.eu>
2026-01-06 22:53:56 +0100

    Add text and styling on the start page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Home.vue</h1>
+  <h2 class="text-3xl m-5 text-center">Laboration 3</h2>
+  <p class="text-1xl lg:text-2xl max-w-8/10 md:max-w-9/10 lg:max-w-6/10 mx-auto my-2">
+    Musik generellt är något som har väldigt stor betydelse för mig och jag
+    har nästan alltid musik på när jag gör något vid datorn eller när jag är
+    ute och går. Jag tycker att det är något speciellt med hur musik kan användas
+    för att förmedla känslor och koppla samman folk.
+  </p>
+  <p class="text-1xl lg:text-2xl max-w-8/10 md:max-w-9/10 lg:max-w-6/10 mx-auto my-2">
+    Många samlar på vinyl-skivor då de påstår att de har överlägsen kvalitet över
+    andra format, vilket det finns en sanning viss sanning i, dock har jag
+	personligen inget emot andra format utan gillar bara att fysiskt kunna
+    äga musiken samt så är det ett sätt att stödja band/artister jag gillar.
+  </p>
 </template>
 
 <style scoped></style>
